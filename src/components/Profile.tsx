645c8e63a6090403923157d84aec7a793076eecc
kimjihu0 <sh9732626@gmail.com>
2024-03-14 23:36:55 +0900

    Component에 Data 뿌려주는 방법 및 조건부 랜더링

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-// Component에 Data 뿌려주는 방법 및 조건부 랜더링
+// Component에 Data 뿌려주는 방법 및 조건부 랜더링!
 // React function에서 데이터를 사용하기 위헤서는 {} 을 사용해야한다.
 export default function Profile({ user }) {
     // 조건부 랜더링 영역, 삼항연산자로도 가능
