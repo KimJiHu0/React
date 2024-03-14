66fe1d770c9d94566852d1161c109dd45320531b
kimjihu0 <sh9732626@gmail.com>
2024-03-14 23:43:16 +0900

    이벤트 응답하기

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,23 @@
 export default function Button(props) {
     console.log(props);
     // 내부 함수로 이벤트 응답하기
-    function clickFunction(data) {
+
+    // fn1() 으로 호출할 시 return 을 해주는 함수가 필요하다.
+    // onClick는 함수를 넣어줘야 하기 때문이다.
+    function fn1(data) {
         return () => {
             console.log(data);
         };
     }
-    // onClick={(props) => alert(props)}
+    // fn2는 자체가 함수이며 return은 void이다. onClick은 함수를 넣어줘야하기 때문에 fn2 자체를 넣어준다.
+    function fn2() {
+        console.log('Click2');
+    }
     // 괄호는 무조건 빼고 사용해야한다.
-    return <button onClick={clickFunction(props.data)}>Click</button>;
+    return (
+        <>
+            <button onClick={fn1(props.data)}>Click1</button>
+            <button onClick={fn2}>Click2</button>
+        </>
+    );
 }
--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,8 +1,7 @@
 // CSS Import
 import '../assets/css/component.css';
 
-// export default이름을 지정할 경우 해당 Component를 import 하는 Component에서는
-// import Input from '...' 으로 해야한다.
+// CSS 적용하기 및 JSX 마크업 작업하기
 export default function Component({ id }) {
     return (
         // for의 역할은 input의 id를 동일하게 맞춰 놓으면 라벨 클릭 시 input에 focus를 가도록 처리
