f368ec9d828b6b67ca7079f178a580d118720d80
kimjihu0 <sh9732626@gmail.com>
2024-03-19 18:51:37 +0900

    Import & Export

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // 하나의 컴포너느 내에서 export default와 export named를 동시에 사용할 수 있다.
-import Gallery, { Profile } from './components/Gallery';
+import Gallery, { Profile } from './components/ImportExport/Gallery';
 // 여러 개의 컴포넌트가 있는 경우 대표 component만 import하여 아래처럼 컴포넌트명.함수명 으로 불러서 사용할 수 있다.
-import { Input } from './components/Input';
+import { Input } from './components/ImportExport/Input';
 export default function App() {
     return (
         <>
+            <h1>Component Import & Export</h1>
             <p>Gallery Import</p>
             <Gallery />
             <p>Gallery/Profile Import</p>
@@ -13,6 +14,7 @@ export default function App() {
             <Input />
             <p>이렇게 Input.tsx 안에 별로도 생성된 컴포넌트도 사용 가능하다.</p>
             <Input.Input />
+            <hr />
         </>
     );
 }
