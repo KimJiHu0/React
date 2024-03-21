914c3dddf049a6006cf04d2c5b6c762e46b2509c
kimjihu0 <sh9732626@gmail.com>
2024-03-21 18:16:35 +0900

    React Render

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
 // 동작하지 않음
 import { useState } from 'react';
 
+// 버튼을 클릭하면 상위의 State가 변경되고 이를 props로 전달받은 Button1 컴포넌트는
+// 버튼을 클릭 시 리랜더링이 된다.
 const Button1 = ({ index, handleClick }) => {
     return (
-        <p>
-            {index} <button onClick={handleClick}>++</button>
-        </p>
-    );
-};
-
-const Button2 = () => {
-    const [index, setIndex] = useState(0);
-    const handlePlus = () => {
-        setIndex(index + 1);
-    };
-
-    return (
-        <p>
-            {index} <button onClick={handlePlus}>++</button>
-        </p>
-    );
-};
-
-// State를 set 후 바로 사용하면 안됨.
-const Input = () => {
-    const [name, setName] = useState('');
-    const onInputClick = () => {
-        setName(prompt('이름을 입력해주세요.'));
-        console.log(name);
-    };
-    return (
         <>
-            {name}
-            <button onClick={onInputClick}>이름 입력하기</button>
+            {index} <button onClick={handleClick}>++</button>
         </>
     );
+};
+
+// props로 상위의 State를 전달받지 않은 Text 컴포넌트는 State가 변경이 되더라도
+// 리랜더링이 되지 않는다.
+const Text = () => {
+    return <p>'Hi, LEE!'</p>;
 };
 
 export default function App() {
@@ -44,16 +24,8 @@ export default function App() {
     };
     return (
         <>
-            <h1>State를 공유하는 방법</h1>
             <Button1 index={index} handleClick={handleClick} />
-            <Button1 index={index} handleClick={handleClick} />
-            <hr />
-            <h1>State를 컴포넌트 각각 사용하는 방법</h1>
-            <Button2 />
-            <Button2 />
-            <hr />
-            <h1>State후 바로 출력하면 안되는 이유</h1>
-            <Input />
+            <Text />
         </>
     );
 }
