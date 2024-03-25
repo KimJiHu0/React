4d6adb06faaae328dd52ec948e2f62780ac97973
kimjihu0 <sh9732626@gmail.com>
2024-03-25 13:20:36 +0900

    React State로 입력에 대한 반응하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import Sample1 from './assets/components/Sample1';
-import Sample2 from './assets/components/Sample2';
-import Sample3 from './assets/components/Sample3';
-import Sample4 from './assets/components/Sample4';
+import { useState } from 'react';
+
+const Profile = () => {
+    // 이름을 담는 State
+    const [{ fristName, lastName }, setObjectValues] = useState({ fristName: '', lastName: '' });
+    // 수정중이면 true, 아니면 false
+    const [isEditing, setIsEditing] = useState(false);
+
+    // input의 change이벤트 함수
+    const onInputChange = (e) => {
+        setObjectValues((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    // form의 submit이벤트 함수
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        setIsEditing(!isEditing);
+    };
+
+    return (
+        <>
+            <h2>Profile</h2>
+            <form onChange={onInputChange} onSubmit={onFormSubmit}>
+                <label>FristName : {isEditing ? <input name="fristName" /> : <b>{fristName}</b>}</label>
+                <br />
+                <label>LastName : {isEditing ? <input name="lastName" /> : <b>{lastName}</b>}</label>
+                <div>
+                    <button>{isEditing ? 'Save' : 'Edit Profile'}</button>
+                </div>
+                <p>
+                    <i>
+                        Hi, {fristName} {lastName}
+                    </i>
+                </p>
+            </form>
+        </>
+    );
+};
+
 export default function App() {
     return (
         <>
-            {/* <h1>객체 변경하는 방법</h1> */}
-            <Sample1 />
+            <h1>State에 따른 상태 변화</h1>
+            <Profile />
             <hr />
-            <h1>필요한 부분만 객체 변경하는 방법</h1>
-            <Sample2 />
-            <hr />
-            <h1>배열 객체 업데이트</h1>
-            <Sample3 />
-            <hr />
-            <h1>예제 풀기</h1>
-            <Sample4 />
         </>
     );
 }
