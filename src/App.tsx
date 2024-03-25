160d5973f0513a37ab6d593cf980474736ee3afd
kimjihu0 <sh9732626@gmail.com>
2024-03-25 18:05:53 +0900

    React State 공유하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,42 @@ import Sample1 from './assets/components/Sample1';
 import Sample2 from './assets/components/Sample2';
 import Sample3 from './assets/components/Sample3';
 import Sample4 from './assets/components/Sample4';
+import { useState } from 'react';
 
 export default function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <>
-            <h1>State 그룹화</h1>
-            <Sample1 />
+            <h1>컴포넌트 간에 State 공유</h1>
+            <label htmlFor="name">
+                <button
+                    id="name"
+                    onClick={(e) => {
+                        setCount(count + 1);
+                    }}
+                >
+                    +
+                </button>
+                <button
+                    id="name"
+                    onClick={(e) => {
+                        setCount(count - 1);
+                    }}
+                >
+                    -
+                </button>
+            </label>
+            <h4>Component 1</h4>
+            <Sample1 count={count} />
+            <h4>Component 2</h4>
+            <Sample1 count={count} />
             <hr />
-            <h1>State 모순 피하기</h1>
+            <h1>컴포넌트 간에 State 분리</h1>
+            <h6>Component 1</h6>
             <Sample2 />
-            <hr />
-            <h1>State 중복 피하기</h1>
-            <Sample3 />
-            <hr />
-            <h1>깊은 계층 피하기</h1>
-            <Sample4 />
+            <h6>Component 2</h6>
+            <Sample2 />
         </>
     );
 }
