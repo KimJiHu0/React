6eebc190ad008f9ef5e68cf84e85bf192982abf3
kimjihu0 <sh9732626@gmail.com>
2024-03-28 18:32:57 +0900

    React ref를 제어할 때 useEffect를 쓰고 안쓰고의 차이

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,15 @@
 import Sample1 from './assets/components/Sample1';
-import Sample2 from './assets/components/Sample2';
-import Sample3 from './assets/components/Sample3';
-import { Sample4 } from './assets/components/Sample4';
-import Sample5 from './assets/components/Sample5';
-import Sample6 from './assets/components/Sample6';
-import Sample7 from './assets/components/Sample7';
-import Sample8 from './assets/components/Sample8';
 
-import { useRef } from 'react';
+import { useState } from 'react';
 
 export default function App() {
-    const inputRef = useRef(null);
-    const inputRef2 = useRef(null);
-    const inputForwardRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
     return (
         <>
-            <h1>예제1 : Input에 초점 맞추기</h1>
-            <Sample1 />
-            <p>
-                useRef를 사용하면 초기에는 Null이 들어가는데, React가 해당 HTML의 노드를 생성하면서 선언된 Ref의
-                current에 넣는다. 그 후 DOM 노드에 엑세스하고 정의된 빌트인 브라우저 API를 사용할 수 있다.
-            </p>
-            <hr />
-            <h1>예제2 : Element 스크롤하기</h1>
-            <Sample2 />
-            <p>
-                버튼을 클릭했을 때 원하는 DOM이 담긴 Element를 가운데로 오게하기 위해서는 'scrollIntoView' 함수를 사용할
-                수 있다.
-            </p>
-            <hr />
-            <h1>예제3 : 동적 List일 경우 처리하는 방법</h1>
-            <Sample3 />
-            <p>
-                만일 동적으로 변하는 List에 대한 처리를 할 수 있다. ref에는 어떤 값이라도 담을 수 있기 때문에 노드들을
-                key, value로 담아준다. 그 후 클릭 시 해당 노드로 scrollIntoView해주면 된다. ref에 콜백함수를 작성하여
-                처리할 수 있다.
-            </p>
-            <hr />
-            <h1>다른 컴포넌트의 DOM 노드에 접근하기</h1>
-            <h3>Ref</h3>
-            <Sample4.UseRef ref={inputRef} />
-            <button
-                onClick={() => {
-                    inputRef.current.focus();
-                }}
-            >
-                포커스
-            </button>
-            <Sample4.UseRef2 inputRef2={inputRef2} />
-            <button
-                onClick={() => {
-                    inputRef2.current.focus();
-                }}
-            >
-                포커스
-            </button>
-            <p>
-                직접 선언한 Component에는 ref로 props를 전달할 수 없다. 그래서 다른 DOM에 접근하려면 forwardRef를
-                사용한다. 다만 ref가 아닌 다른 props의 이름을 사용하게 되면 가능은 합니다.
-            </p>
-            <h3>forwardRef</h3>
-            <Sample4.UseForwardRef ref={inputForwardRef} />
-            <button
-                onClick={() => {
-                    inputForwardRef.current.focus();
-                }}
-            >
-                포커스
-            </button>
-            <hr />
-            <h1>도전과제</h1>
-            <h2>할 일 목록 만들기</h2>
-            <Sample5 />
-            <hr />
-            <h2>동영상 재생/정지 하기</h2>
-            <Sample6 />
-            <hr />
-            <h2>이미지 캐러셀 스크롤하기</h2>
-            <Sample7 />
-            <hr />
-            <h2>각각의 컴포넌트로 검색필드 초점 맞추기</h2>
-            <Sample8 />
+            <h1>useEffect의 코드는 매 랜더링 시 발생한다.</h1>
+            <Sample1 isPlaying={isPlaying} />
+            <button onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? 'Pause' : 'Play'}</button>
+            <p>useEffect를 사용했을 경우와 안했을 경우 ref를 사용할 때에는 차이점을 확인할 수 있다.</p>
         </>
     );
 }
--- src/assets/components/Sample1.tsx
+++ src/assets/components/Sample1.tsx
@@ -1,21 +1,35 @@
-import { useRef } from 'react';
-export default function Sample1() {
-    const inputRef = useRef(null);
+import { useRef, useEffect } from 'react';
+export default function Sample1({ isPlaying }) {
+    const videoRef = useRef(null);
+    // isPlaying : true => 플레이 중이므로 Pause 버튼이 보여지며, 클릭 시 정지
+    // isPlaying : false => 정지 중이므로 Play 버튼이 보여지며, 클릭 시 실행
 
-    const onFocus = () => {
-        inputRef.current.focus();
-    };
+    // useEffect를 사용하지 않을 경우 랜더링 전에 조건문 로직이 실행되어
+    // ref에는 null이 담겨있는 채로 로직을 실행하려 하기 때문에 에러가 발생한다.
+    // if (isPlaying) {
+    //     videoRef.current.play();
+    // } else {
+    //     videoRef.current.pause();
+    // }
+
+    // useEffect를 사용할 경우 해당 로직은 랜더링이 된 후에 실행되기 때문에
+    // ref에는 video HTML이 담겨있어 정상적으로 실행할 수 있다.
+    useEffect(() => {
+        if (isPlaying) {
+            videoRef.current.play();
+        } else {
+            videoRef.current.pause();
+        }
+    });
 
     return (
-        <>
-            <input ref={inputRef} />
-            <button
-                onClick={() => {
-                    onFocus();
-                }}
-            >
-                Focus
-            </button>
-        </>
+        <div>
+            <video width="250" ref={videoRef}>
+                <source
+                    src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+                    type="video/mp4"
+                />
+            </video>
+        </div>
     );
 }
